9ed4e57b3cfb75c4f30d67f649b40713e3a7528e
Rafał Grodziński <rafal.grodzinski@gmail.com>
2025-04-25 14:56:18 +0900

    Fixed skybox

--- src/shaders/skybox/skybox_vertex.glsl
+++ src/shaders/skybox/skybox_vertex.glsl
@@ -8,8 +8,10 @@ uniform mat4 u_viewMatrix;
 out vec3 v_texCoords;
 
 void main() {
-    v_texCoords = vec3(a_position.x, a_position.y, -a_position.z);
+    v_texCoords = a_position;
 
-    vec4 position = vec4(a_position, 1.0) * mat4(mat3(u_viewMatrix)) * u_projectionMatrix; 
+    mat4 projection = u_projectionMatrix;
+    projection[3][3] = 0.0;
+    vec4 position = vec4(a_position, 1.0) * mat4(mat3(u_viewMatrix)) * projection;
     gl_Position = position.xyww;
 }
